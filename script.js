d27bd5f4c8940feb2c3465ccf2ceefee47547ff1
ali-aboulsauood <aliaboulsauood@gmail.com>
2025-02-12 23:57:56 +0200

    Add function to get computer choice
    TODO:
        - Use an array or a map to help replace the `switch` statement in the function body with simpler code.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const NUMBER_OF_CHOICES = 3;
+
+function getComputerChoice() {
+    // refer to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_number_between_two_values for more information about how the following statement works.
+
+    let computerChoice = parseInt(Math.random() * NUMBER_OF_CHOICES + 1);
+
+    // TODO: Use an array or a map to replace the following `switch` statement.
+
+    switch (computerChoice) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+
+        // Execution can never reach the default case. Added for completeness.
+        default:
+            return "";
+    }
+}
